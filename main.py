0569c02be15feb147e943ec84b2eeee971e1c839
cynicalico <228817350+cynicalico@users.noreply.github.com>
2025-11-10 16:59:31 -0700

    added vinegar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
 import os
 import time
+import pickle
 
 import filetype
 import mutagen
@@ -9,39 +9,48 @@ import xxhash
 MUSIC_LIBRARY_PATH = r"C:\Users\bofeh\Dev\tmp\citrus_test_collection"
 
 
+class AlbumListing:
+    def __init__(self, name):
+        self.name = name
+        self.track_ids = []
+
+    def add_track(self, track_hash):
+        self.track_ids.append(track_hash)
+
+
+class ArtistListing:
+    def __init__(self, name):
+        self.name = name
+        self.albums = {}
+
+    def check_add_album(self, album):
+        if album not in self.albums:
+            self.albums[album] = AlbumListing(album)
+
+    def check_add_track(self, album, track_hash):
+        self.check_add_album(album)
+        self.albums[album].add_track(track_hash)
+
+
 class LibraryDb:
     def __init__(self):
-        self.data = {
-            "lookup_artist": {},
-            "lookup_hash": {}
-        }
+        self.artists = {}
+        self.hash_db = {}
 
     def check_add_artist(self, artist):
-        if artist not in self.data["lookup_artist"]:
-            self.data["lookup_artist"][artist] = {
-                "albums": {},
-                "tracks": [],
-            }
+        if artist not in self.artists:
+            self.artists[artist] = ArtistListing(artist)
 
     def check_add_album(self, artist, album):
         self.check_add_artist(artist)
+        self.artists[artist].check_add_album(album)
 
-        if album not in self.data["lookup_artist"][artist]["albums"]:
-            self.data["lookup_artist"][artist]["albums"][album] = {
-                "tracks": [],
-            }
-
-    def check_add_track(self, artist, album, track, md5):
-        if album is None:
-            self.check_add_artist(artist)
-            self.data["lookup_artist"][artist]["tracks"].append((track, md5))
-
-        else:
-            self.check_add_album(artist, album)
-            self.data["lookup_artist"][artist]["albums"][album]["tracks"].append((track, md5))
+    def check_add_track(self, artist, album, track, track_hash):
+        self.check_add_album(artist, album)
+        self.artists[artist].check_add_track(album, track_hash)
 
         # TODO: log collisions
-        self.data["lookup_hash"][md5] = (artist, album, track)
+        self.hash_db[track_hash] = (artist, album, track)
 
 
 def get_hash(path):
@@ -66,7 +75,15 @@ class Citrus:
                         album = album[0]
                     self.db.check_add_track(artist, album, path, get_hash(path))
 
-        json.dump(self.db.data, open("db.json", "w"), indent=4)
+        for artist in self.db.artists.values():
+            print(f"Artist: {artist.name}")
+            for album in artist.albums.values():
+                print(f"\tAlbum: {album.name}")
+                for track in album.track_ids:
+                    print(f"\t\tTrack: {self.db.hash_db[track]}")
+
+        with open("db.pickle", "wb") as f:
+            pickle.dump(self.db, f)
 
 
 if __name__ == '__main__':
